664092e721db3590de7dda1e3ac20cfcf52cecf0
Katya <riscocalin@gmail.com>
2023-11-27 19:57:07 +0200

    key press

---
+++ src/main/java/Pages/KeyPressPage.java
@@ -0,0 +1,32 @@
+package Pages;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import javax.swing.*;
+
+public class KeyPressPage extends BasePage {
+    //=====================================================
+    public KeyPressPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    //=====================================================
+    @FindBy(id = "my_field")
+    private WebElement textField;
+
+    public void typeInTheTextField(String text) {
+        textField.sendKeys(text);
+    }
+    public void typeInShiftKey(){
+        textField.click();
+        Actions action = new Actions(driver);
+        action.keyDown(Keys.SHIFT).perform();
+    }
+
+}
---
+++ src/test/java/KeyPressTests.java
@@ -0,0 +1,32 @@
+import Pages.BasePage;
+import Pages.KeyPressPage;
+import jdk.jfr.Description;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class KeyPressTests extends BasePage {
+    private KeyPressPage keyPressPage;
+    private static final By KEY_PRESS_LINK_TEXT = By.linkText("Key Press");
+
+    @BeforeMethod
+    public void setUp() {
+        super.setUp();
+        keyPressPage = new KeyPressPage(driver);
+
+
+        @Description("Write a letter and check the result")
+
+        @Test
+        public void writeALetterAndCheckTheResult(){
+            driver.findElement(KEY_PRESS_LINK_TEXT).click();
+            keyPressPage.typeInTheTextField("T");
+            String actualResult = driver.findElement(By.id("result")).getText();
+            Assert.assertEquals(actualResult, "You entered:ALT");
+
+
+        }
+
+    }
+}
